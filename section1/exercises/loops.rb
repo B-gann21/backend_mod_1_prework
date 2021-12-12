ee4e6f3204d48f581b92c6dd72e655cfffa76ce1
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-12 12:38:12 -0700

    Fix typo in loops.rb

--- section1/exercises/loops.rb
+++ section1/exercises/loops.rb
@@ -10,14 +10,14 @@ end
 
 # Write code that prints the sum of 2 plus 2 seven times:
 7.times do
-  p 7 + 7
+  p 2 + 2
 end
 
 # Write code that prints the phrase 'She sells seashells down by the seashore'
 # ten times:
 # YOUR CODE HERE
 10.times do
-  p "She sells seashells down by the seahore"
+  p "She sells seashells down by the seashore"
 end
 
 # Write code that prints the result of 5 + 7 a total of 9 timees
